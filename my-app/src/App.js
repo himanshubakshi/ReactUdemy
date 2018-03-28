6578ca3145fb1d13d9cad74033b02bef2382e206
Bakshi, Himanshu <himanshu_bakshi@intuit.com>
2018-03-28 12:30:36 +0530

    react create element instead of JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    
+    //return (
+      // <div className="App">
+      //     <h1>Hi, i am a react app.</h1>
+      // </div>
+      //);
 
-      <div className="App">
-          <h1>Hi, i am a react app.</h1>
-      </div>
-      
-    );
+
+     //return React.createElement('div', null, 'h1','Hi, i too am a react app.');
+
+    //  return React.createElement('div', null, React.createElement('h1', null, 'Hi, I too am a react app.'), React.createElement('h1', null, 'Hi, Am i the next child of div? I sure am.'));
+     return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I too am a react app.'), React.createElement('h1', null, 'Hi, Am i the next child of div? I sure am.'));
+
   }
 }
 
