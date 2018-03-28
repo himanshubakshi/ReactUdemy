1e79aa6a0800646456f8179bb8b1b0714b732fe4
Bakshi, Himanshu <himanshu_bakshi@intuit.com>
2018-03-28 13:06:07 +0530

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// any name can be given here, it is an alias. It's the default export from the file
+import Person from './Person/Person';
+
 class App extends Component {
   render() {
     
-    //return (
-      // <div className="App">
-      //     <h1>Hi, i am a react app.</h1>
-      // </div>
-      //);
+    return (
+      // class can't be used for css because it's used in js, to define the class
+      // we need to use react provided keywords
+      <div className="App">
+          <h1>Hi, I am a react app!</h1>
+
+          <p> The app works</p>
+
+          <br/>
+
+          <hr/>
+          <Person name={"Himanshu Bakshi"} age={29} />
+          <hr/>
+          <Person name={"John Doe"} age={99} />     
+          <hr/>
+          <Person name={"The Rock"} age={35} />
+          <hr/>
+          
+      </div>
+      );
 
 
-     //return React.createElement('div', null, 'h1','Hi, i too am a react app.');
-
-    //  return React.createElement('div', null, React.createElement('h1', null, 'Hi, I too am a react app.'), React.createElement('h1', null, 'Hi, Am i the next child of div? I sure am.'));
-     return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I too am a react app.'), React.createElement('h1', null, 'Hi, Am i the next child of div? I sure am.'));
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I too am a react app.'), React.createElement('h1', null, 'Hi, Am i the next child of div? I sure am.'));
 
   }
 }
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -8,9 +8,13 @@ const person = (props) => {
             <p>I'm a person!</p>
             <p>Name: {props.name}</p>
             <p>Age: {props.age}</p>
+            <p>My son is {Math.floor(Math.random() * 20)} years old</p>
         </div>
     );
 };
+
+// {} inside html (read jsx defined html in the retuen statement) can execute one line statements, function calls. 
+// We can't create classes and do other complex stuff...}
 
 export default person;
 
