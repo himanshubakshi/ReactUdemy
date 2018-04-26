ed7d9a9d92859fc7f5251852d18fb0bed7acd262
Bakshi, Himanshu <himanshu_bakshi@intuit.com>
2018-04-26 16:41:10 +0530

    assignment 2 complete

--- my-react-assignment-2/src/App.js
+++ my-react-assignment-2/src/App.js
@@ -1,36 +1,79 @@
 import React, { Component } from 'react';
+import ValidationComponent from './Components/ValidationComponent'
+import CharComponent       from './Components/CharComponent'
 import './App.css';
+
 
 class App extends Component {
   
   state = {
-    textLength: 0
+    textLength: 0,
+    text: ''
   };
 
   onTextChangeListener = (event) => {
       let textLength = event.target.value.length;
 
       this.setState({
-        textLength: textLength
+        textLength: textLength,
+        text: event.target.value
       });
-  }
+  };
 
+  charClickHandler = (event, indexOfChar) => {
+
+      console.log(indexOfChar);
+
+      const enteredText = this.state.text;
+
+      let newValue = enteredText.split('').map((value, index) => { 
+        return  (index != indexOfChar) ? value : null
+      }).join('');
+
+
+      console.log(newValue);
+
+      this.setState({
+        text: newValue,
+        textLength: newValue.length
+      });
+  };
 
 
   render() {
+
     return (
       <div className="App">
-       Assignment 2
+         Assignment 2
 
-        <br/>
-        <div>
+          <br/>
+          <div>
 
-          <input type="text" 
-                  onChange={this.onTextChangeListener}
-                  />
-          <p>Text Length: {this.state.textLength}</p>
+            <input type="text" 
+                    onChange={this.onTextChangeListener}
+                    value={this.state.text}
+                    />
+            <p>Text Length: {this.state.textLength}</p>
 
-        </div>
+          </div>
+
+          <div>
+              <ValidationComponent textLength={this.state.textLength}/>
+          </div>
+
+
+          <div> 
+
+              {this.state.text.split("").map((value, index) => {
+                return  <CharComponent charValue={value}
+                                        indexOfChar={index} 
+                                        key={index}
+                                        charClickHandler={(event, indexOfChar) => this.charClickHandler(event, indexOfChar)}/>
+              })}
+
+
+             
+          </div>
       </div>
     );
   }
